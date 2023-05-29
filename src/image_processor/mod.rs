f7de09afa94b12db524c6afbec01801bcccd665b
Jason Yuan <jason.yuan869@gmail.com>
2023-05-29 18:25:10 -0400

    cleanup: remove unused imports

--- src/image_processor/mod.rs
+++ src/image_processor/mod.rs
@@ -2,7 +2,6 @@ use std::path::Path;
 
 use image::{GenericImageView, RgbImage};
 use image::imageops::overlay;
-use rayon::prelude::*;
 
 use crate::image_processor::colors::{BLACK_INDEX, MapColor, MINECRAFT_COLOR_TREE};
 
